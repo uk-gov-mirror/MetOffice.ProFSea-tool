3205e1f60f708545ac5c24233e9b5b0ce3c7cfa9
Gregory Munday <gregory.munday@gmail.com>
2026-04-21 11:20:38 +0100

    Add Global model save method back in

--- test_build.py
+++ test_build.py
@@ -15,3 +15,4 @@ model = Global(
 
 projections = model.run(scenario="test", T_change=np.arange(295).reshape(1, -1), OHC_change=np.arange(295).reshape(1, -1), member_seed=42)
 gmslr = model.sum_components(projections)
+model.save_components("test_output", "test_projection")
